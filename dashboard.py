370051130c7b6c72fbc5d11113a5ea599c32e0fb
sdowd <sarahlynnedowd@gmail.com>
2025-12-02 20:23:04 -0500

    add UX style to data-table and data-table filter callback built

--- dashboard.py
+++ dashboard.py
@@ -33,23 +33,61 @@ app.layout = html.Div([
         dbc.Label('Shelter Data'),
         # dropdown menu with rescue options to filter data displayed in table, placeholder text instructs user
         dcc.Dropdown(['Water Rescue', 'Mountain Rescue', 'Disaster Rescue', 'Reset'],
-                     placeholder='Select Rescue Type to Filter Data'),
+                     placeholder='Select Rescue Type to Filter Data', id='dropdown-filter'),
         dash_table.DataTable(data=df.to_dict('records'),
                             columns=[{'name' : i, 'id' : i, 'deletable' : False, 'selectable' : True} for i in df.columns],
-                            row_selectable='single', page_action='native', page_size=15, page_current=0, id='shelter-table')
+                            row_selectable='single', page_action='native', page_size=15, page_current=0, id='shelter-table',
+                            style_cell={'textAlign' : 'left', 'height': 'auto',
+                            'minWidth': '180px', 'width': '180px', 'maxWidth': '180px',
+                            'whiteSpace': 'normal'}, style_table={'overflowX': 'auto'})
     ]),
-    html.Br,
+    html.Br(),
     html.Div(id='map-div'),
     html.Br(),
     html.Br(),
 
 ])
 
+# callback to update datatable by filtering data based on dropdown selection
+@app.callback(
+    Output('shelter-table', 'data'),
+    [Input('dropdown-filter', 'value')]
+)
+def update_table(dropdown_filter):
+    # create query for each dropdown filter based on given Grazioso Salvare criteria
+    if dropdown_filter == 'Water Rescue':
+        query = {
+            'breed': {'$in': ['Labrador Retriever Mix', 'Chesapeake Bay Retriever', 'Newfoundland']},
+            'sex_upon_outcome': 'Intact Female',
+            'age_upon_outcome_in_weeks': {'$gte': 26, '$lte': 156}
+        }
+    elif dropdown_filter == 'Mountain Rescue':
+        query = {
+            'breed': {'$in': ['German Shepherd', 'Alaskan Malamute',
+                              'Old English Sheepdog', 'Siberian Husky', 'Rottweiler']},
+            'sex_upon_outcome': 'Intact Male',
+            'age_upon_outcome_in_weeks': {'$gte': 26, '$lte': 156}
+        }
+    elif dropdown_filter == 'Disaster Rescue':
+        query = {
+            'breed': {'$in': ['Doberman Pinscher', 'German Shepherd',
+                              'Golden Retriever', 'Bloodhound', 'Rottweiler']},
+            'sex_upon_outcome': 'Intact Male',
+            'age_upon_outcome_in_weeks': {'$gte': 20, '$lte': 300}
+        }
+    # reset option passes in empty query as in first df construct
+    else:
+        query = {}
+    # pass dropdown_filter query into db.read to create new filtered dataframe & drop objectid
+    filtered_df = pd.DataFrame.from_records(db.read(query))
+    filtered_df.drop(columns=['_id'], inplace=True)
+    # return filtered dataframe to data property of DataTable
+    return filtered_df.to_dict('records')
 
 @app.callback(
     Output('map-div', "children"),
-    [Input('datatable-id', "derived_virtual_data"),
-     Input('datatable-id', "derived_virtual_selected_rows")])
+    [Input('shelter-table', "derived_virtual_data"),
+     Input('shelter-table', "derived_virtual_selected_rows")])
 def update_map(viewData, index):
     if viewData is None:
         return None
